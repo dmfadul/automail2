100e0df8cdce6f0f91b3c01e85632679519fc6da
drMatterhorn <doctormatterhorn@gmail.com>
2024-08-29 16:39:00 -0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import funcs
 
 subject_text = "Certidão de Conclusão de Curso - "
-main_text = ''
+main_text = 'Boa tarde, segue o certificado retificado referente à palestra sobre Desafios Operacionais no Combate ao Crime Organizado. Pedimos que desconsiderem o certificado anterior, tendo em vista que o curso e a carga horária estavam incorrretos.'
 
 
 funcs.multi_send(subject_text=subject_text, main_text=main_text, restart=False)
